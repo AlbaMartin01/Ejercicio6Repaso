f60464997319b76922c309bde344b7d41da7a987
equus2001 <62957466+equus2001@users.noreply.github.com>
2022-12-19 11:56:20 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ejercicio6repaso/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.ejercicio6repaso
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var button: Button
+    lateinit var editText: EditText
+    lateinit var imageView: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button_start)
+        editText = findViewById(R.id.edit_text_name)
+        imageView = findViewById(R.id.image_view_logo)
+
+        button.setOnClickListener {
+            if (editText.visibility == View.GONE) {
+                editText.visibility = View.VISIBLE
+                button.text = "Finalizar"
+            } else {
+                editText.visibility = View.GONE
+                button.text = "Comenzar"
+            }
+        }
+
+        editText.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                editText.hint = ""
+            } else {
+                if (editText.text.isEmpty()) {
+                    editText.hint = "Introduce tu nombre"
+                }
+            }
+        }
+
+            editText.setOnFocusChangeListener { view, hasFocus ->
+                if (!hasFocus && editText.text.toString().isEmpty()) {
+                    editText.setText("Introduce tu nombre")
+                }
+            }
+
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            @SuppressLint("ResourceAsColor")
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.toString().contains("Wayne", ignoreCase = true)) {
+                    imageView.visibility = View.VISIBLE
+                } else {
+                    imageView.visibility = View.GONE
+                }
+
+                if (s.toString().contains("Joker", ignoreCase = true)) {
+                    val color = ContextCompat.getColor(applicationContext, R.color.colorJocker)
+                    editText.setTextColor(R.color.white)
+                    button.setTextColor(color)
+                    window.decorView.setBackgroundColor(color)
+                } else {
+                    val color = ContextCompat.getColor(applicationContext, android.R.color.black)
+                    editText.setTextColor(R.color.white)
+                    button.setTextColor(color)
+                    window.decorView.setBackgroundColor(color)
+                }
+            }
+        })
+    }
+}
